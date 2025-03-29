0ab356b62f84c27a0a43ac28817717d0efca2613
Unigwe <emmunigwe@gmail.com>
2025-03-29 15:53:59 +0100

    testing husty for mobile 3

---
+++ Eyria-mobile/eslint.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,tsx}"] },
+  { files: ["**/*.{js,mjs,cjs,ts,tsx}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
+]);
