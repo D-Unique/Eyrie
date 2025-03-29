8e2231f4a6aeb2b14c4f09a8e46ef8cd9aafd30f
Unigwe <emmunigwe@gmail.com>
2025-03-29 19:02:42 +0100

    all

--- Eyria-mobile/eslint.config.mjs
+++ Eyria-mobile/eslint.config.mjs
@@ -5,8 +5,8 @@ import tseslint from "typescript-eslint";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   tseslint.configs.recommended,
 ]);
