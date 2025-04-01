a16e8c5cfa6149704b50dceaed420c10da157311
Unigwe <emmunigwe@gmail.com>
2025-04-01 23:43:01 +0100

    perf(docker): dockerised monoserver code

--- MonoServer/app/server.ts
+++ MonoServer/app/server.ts
@@ -1,15 +1,14 @@
-import express, { Router } from "express";
+import { Request, Response} from 'express';
+import express from "express"
+
+// Correctly typing the req and res parameters
 const app = express();
 const port = 3000;
 
-const router = Router();
-
-router.use(a);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
