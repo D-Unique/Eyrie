062a1de869190094e9db42857c8c83b0dd13bcfa
Precios Ezeigbo <preciousezeigo81@gmail.com>
2025-04-23 20:21:51 +0100

    Fix: fixed login background

--- Eyrie-web/src/app/login/page.tsx
+++ Eyrie-web/src/app/login/page.tsx
@@ -24,7 +24,7 @@ export default function Login() {
       {/* Background Image */}
       <div className="hidden md:block absolute inset-0">
         <Image
-          src="/images/login.jpg"
+          src="/images/Login.jpg"
           alt="Background"
           fill
           className="object-cover"
